551e67b54e706d6c1452612cb3065075d6a7179b
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2026-06-19 02:32:09 +0530

    fix: import config in ZoqqAdapter to fix getQuote crash

--- src/providers/zoqq/ZoqqAdapter.js
+++ src/providers/zoqq/ZoqqAdapter.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import { config } from '../../config/index.js';
 import { IPaymentProvider } from '../IPaymentProvider.js';
 import { ZoqqClient } from './ZoqqClient.js';
 import {
